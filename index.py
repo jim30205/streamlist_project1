db762d979d3f2fb1e9faf4b97cd5ea3f3365505a
吳俊逸 <jim30205@gmail.com>
2022-11-15 11:00:28 +0800

    新增firebase資料

--- index.py
+++ index.py
@@ -14,6 +14,10 @@ if db is None:
         app = firebase_admin.initialize_app(cred)
 
     db = firestore.client()
+doc_ref=db.collection("records")
+docs = doc_ref.get()
+for doc in docs:
+    print(doc)
 st.title("光線和距離即時監控")
 def downloadData():
     print("下載資料")
