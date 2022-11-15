975cae9a0ebfed2fee561cea7cf46f7bf630cf2f
吳俊逸 <jim30205@gmail.com>
2022-11-15 10:33:59 +0800

    新增db=none

--- index.py
+++ index.py
@@ -3,13 +3,16 @@ import time
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-
+import json
 # Use a service account.
-cred = credentials.Certificate('private/raspberry1-d07d8-firebase-adminsdk-4tlwq-68582b8dbd.json')
+db =None
+if db is None:
+    key_dict=json.loads(st.secrets["textkey"])
+    cred = credentials.Certificate('private/raspberry1-d07d8-firebase-adminsdk-4tlwq-68582b8dbd.json')
 
-app = firebase_admin.initialize_app(cred)
+    app = firebase_admin.initialize_app(cred)
 
-db = firestore.client()
+    db = firestore.client()
 st.title("光線和距離即時監控")
 def downloadData():
     print("下載資料")
